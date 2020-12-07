ac0b4defed8f37951d50ce9ba0e4f4e81a2d0d44
YuliiaOsadchukkk <72582368+YuliiaOsadchukkk@users.noreply.github.com>
2020-12-07 02:23:13 +0200

    Lab_8.1.1_UT

---
+++ Lab_8.1.1__UT/Lab_8.1.1__UT.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_8.1.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab811UT
+{
+	TEST_CLASS(Lab811UT)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = { 'z', 'q', 'q', 'q', 'q' };
+
+			bool functionResult = IsOrNot(str);
+
+			Assert::AreEqual(true, functionResult);
+		}
+	};
+}
